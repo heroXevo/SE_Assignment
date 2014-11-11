1269eee41b13b1a6e79789a11e6c45e3c51a7232
heroXevo <matthew.mifsud@mcast.edu.mt>
2014-11-11 13:21:17 +0100

    Connection Class
    Added the Connection class to the Data Access Layer so that i will be
    able to communicate with the database

---
+++ TraderPlaceApp/Data Access/ConnectionClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Common; 
+
+namespace Data_Access
+{
+    public class ConnectionClass
+    {
+
+        public SE_DatabaseEntities entities { get; set; }
+
+        public ConnectionClass()
+        {
+
+            entities = new SE_DatabaseEntities();
+
+        }
+
+    }
+}
