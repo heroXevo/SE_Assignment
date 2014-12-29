cd726ca67e63ea59bf8e84a230f7bdf1eb79b86d
heroXevo <matthew.mifsud@mcast.edu.mt>
2014-12-29 15:55:23 +0100

    Product Management Complete
    The Product Management is Complete this includes Adding a product,
    delete a product, editing a product, and listing the products.

--- TraderPlaceApp/TraderPlaceApp/Controllers/ProductController.cs
+++ TraderPlaceApp/TraderPlaceApp/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Common;
 using Business_Logic;
+using TraderPlaceApp.Models;
 
 namespace TraderPlaceApp.Controllers
 {
@@ -20,6 +21,81 @@ namespace TraderPlaceApp.Controllers
 
             return View("Index", prodList);
         }
+
+        public ActionResult AddProduct()
+        {
+
+            return View(new NewProductModel());
+
+        }
+
+        [HttpPost]
+        public ActionResult AddProduct(NewProductModel np, HttpPostedFileBase file)
+        {
+
+
+            
+            //string filename = System.IO.Path.GetFileName(file.FileName);
+            //string strLocation = HttpContext.Server.MapPath("~/Images");
+            //file.SaveAs(strLocation + @"\" + filename.Replace('+', '_'));
+            
+            
+            
+            Product p = new Product();
+            p.Product_Name = np.productName;
+            p.C_Product_Description = np.productDesc;
+            p.Price = Convert.ToDecimal(np.price);            
+            //p.Image = @"\Images\" + filename;
+            p.Image = "~/Images/na.png";
+            p.UserName = "mattmif";
+
+            new ProductsBL().AddProduct(p);
+
+            return RedirectToAction("Index", "Product");
+
+        }
+
+        public ActionResult EditProduct(int prodID)
+        {
+
+            Product p = new ProductsBL().GetProductByID(prodID);
+            NewProductModel model = new NewProductModel();
+
+            model.productName = p.Product_Name;
+            model.productDesc = p.C_Product_Description;
+            model.price = Convert.ToDouble(p.Price);
+            model.Image = p.Image;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult EditProduct(int prodID, NewProductModel m)
+        {
+            Product oldProd = new ProductsBL().GetProductByID(prodID);
+            Product updatedProd = new Product();
+
+            updatedProd.ProductID = prodID;
+            updatedProd.Product_Name = m.productName;
+            updatedProd.C_Product_Description = m.productDesc;
+            updatedProd.Price = Convert.ToDecimal(m.price);
+            updatedProd.Image = m.Image;
+            updatedProd.UserName = oldProd.UserName;
+
+            new ProductsBL().UpdateProduct(updatedProd);
+
+            return RedirectToAction("Index", "Product");
+
+        }
+
+        public ActionResult Delete(int prodID)
+        {
+
+            new ProductsBL().DeleteProductByID(prodID);
+            return Redirect("Index");
+
+        }
+
 
     }
 }
---
+++ TraderPlaceApp/TraderPlaceApp/Models/NewProductModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Data_Access;
+using Common;
+using Business_Logic;
+
+namespace TraderPlaceApp.Models
+{
+    public class NewProductModel
+    {
+        //
+        // GET: /NewProductModel/
+
+
+        
+        public string productName {get; set;}
+        public string productDesc {get; set;}
+        public double price { get; set; }
+        public string Image { get; set; }
+
+        
+    }
+}
